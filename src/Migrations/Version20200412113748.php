516797bf4d77b76d9f715bbb82e5d83e6bdc9f26
lrejou <louisrejou@yahoo.fr>
2020-05-08 17:59:22 +0200

    new design mobile

--- src/Migrations/Version20200412113748.php
+++ src/Migrations/Version20200412113748.php
@@ -22,7 +22,7 @@ final class Version20200412113748 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE musiques ADD sous_cat VARCHAR(255) NOT NULL');
+        //$this->addSql('ALTER TABLE musiques ADD sous_cat VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
